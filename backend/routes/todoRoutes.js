9f91d2ce1caf7bc3d27157dbef502428335472ba
Jiya Chaurasia <jiyachaurasia25@gmail.com>
2025-10-11 13:56:47 +0530

    Update todoRoutes.js

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {getTodos, createTodo, updateTodo, deleteTodo} = require('../controllers/todoController.js');
 
 router.get('/', getTodos);
+router.get('/search', searchTodos);
 router.post('/todos', createTodo);
 router.put('/:id',updateTodo);
 router.delete('/:id',deleteTodo);
